e63498643a93ade9c161625d4733c6a4391e5c98
Nadeem Parokkot <122343207+nadeemparokkot@users.noreply.github.com>
2024-01-13 19:48:46 +0530

    Delete django3_LoginHomeAuthentication_using_session_UsingUserModel directory

--- django3_LoginHomeAuthentication_using_session_UsingUserModel/sampleapp2/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class SessionLoginInfo(models.Model):
-    name=models.CharField(max_length=250)
-    password=models.CharField(max_length=200)
-
-# Create your models here.
